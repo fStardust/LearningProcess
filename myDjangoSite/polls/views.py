305a5c628319348e7df39eed5fc63535e828a696
fStardust <luqi234@163.com>
2021-12-01 16:32:15 +0800

    c

--- myDjangoSite/polls/views.py
+++ myDjangoSite/polls/views.py
@@ -26,6 +26,7 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
