0a01d3aef436706701a4fc23f120119d16c5bc47
fStardust <luqi234@163.com>
2021-12-20 16:03:31 +0800

    clear

--- weather/qWeather/views.py
+++ weather/qWeather/views.py
@@ -2,4 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the weather index.")
+    output = "Hello, world. You're at the weather index."
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
