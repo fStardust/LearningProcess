2c9748475082aac7783268c7789fa73b04e304e0
fStardust <luqi234@163.com>
2022-03-18 10:11:09 +0800

    clear

--- weather/qWeather/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class WeatherInfo(models.Model):
-    all_info = 'josn'
-    city_id = '昆明'
-
--- weather/weather/myKey.py
+++
@@ -1,4 +0,0 @@
-# GOTO https://dev.qweather.com/ 
-# then ADD your KEY here
-# d260678af4af47c8bba662fba49623f7
-KEY = 'd260678af4af47c8bba662fba49623f7'
